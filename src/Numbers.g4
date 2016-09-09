d013df6d0b7274066ff95ae205455c646b5bd898
jonathan.choy@hotmail.com <jonathan.choy@hotmail.com>
2016-09-09 17:52:12 -0300

    Parte 2 do trabalho corrigido.

--- src/Numbers.g4
+++ src/Numbers.g4
@@ -22,18 +22,38 @@ DIGIT
 :	('0'..'9')
 ;
 
+HEX_MAY
+:	('A'..'F')
+;
+
+HEX_MIN
+:	('a'..'f')
+;
+
 BIN_NUM
 :	BIN_DIGIT+ 'b'
 ;
 
 DEC_NUM
-:	[-+]?[0-9]+'.'?[0-9]+([eE][-+]?[ 0-9]+)?
+:	[-+]?[0-9]+'.'?[0-9]+([eE][-+]?[0-9]+)?
 ;
 
 HEX_NUM
-:	[0][xX](DIGIT+|[a-f]+|[A-F]+)
+:	[0][xX](DIGIT+|HEX_MIN+|HEX_MAY+)
 ;
 
 INT_NUM
 :	(DIGIT+ |  '-' DIGIT+)
+;
+
+SIGNO
+:	'*'|'+'|'/'|'-'|'ˆ'
+;
+
+STATUS
+:	'status'
+;
+
+CLEAR
+:	'clear'
 ;
--- src/Numbers.java
+++ src/Numbers.java
@@ -30,23 +30,76 @@ public class Numbers {
 		}
 		do {
 			tk = lexer.nextToken();
+			double num = 0.0;
+			Double topo1 = 0.0, topo2 = 0.0;
 			switch (tk.getType()) {
 			case NumbersLexer.BIN_NUM:
-				System.out.println("INTEIRO BINARIO: " + tk.getText());
+				num = convertNumbers(tk.getText(), 0);
+				stk.add(num);
 				break;
 			case NumbersLexer.INT_NUM:
-				System.out.println("INTEIRO DECIMAL: " + tk.getText());
+				num = Double.parseDouble(tk.getText());
+				stk.add(num);
 				break;
 			case NumbersLexer.DEC_NUM:
-				System.out.println("REAL DECIMAL: " + tk.getText());
+				num = Double.parseDouble(tk.getText());
+				stk.add(num);
 				break;
 			case NumbersLexer.HEX_NUM:
-				System.out.println("INTEIRO HEXADECIMAL: " + tk.getText());
+				num = convertNumbers(tk.getText(), 1);
+				stk.add(num);
 				break;
-
+			case NumbersLexer.SIGNO:
+				char op = tk.getText().charAt(0);
+				if (stk.size() > 1) {
+					if (op == '*')
+						num = operacaoTokens(stk, 0);
+					else if (op == '+')
+						num = operacaoTokens(stk, 1);
+					else if (op == '/')
+						num = operacaoTokens(stk, 2);
+					else if (op == '-')
+						num = operacaoTokens(stk, 3);
+					else
+						num = operacaoTokens(stk, 4);
+					stk.add(num);
+				}
+				break;
+			case NumbersLexer.STATUS:
+				for (int i = 0; i < stk.size(); i++) {
+					System.out.println(i + " - " + stk.get(i));
+				}
+				break;
+			case NumbersLexer.CLEAR:
+				stk.clear();
+				break;
 			}
-			;
 		} while (tk != null && tk.getType() != Token.EOF);
 
+	}
+
+	private static double convertNumbers(String number, int tipo) {
+		if (!number.isEmpty() || !number.equals("")) {
+			if (tipo == 0)
+				return Integer.parseInt(number.substring(0, number.length() - 1), 2);
+			else
+				return Integer.parseInt(number.substring(2), 16);
+		}
+		return 0;
+	}
+
+	private static double operacaoTokens(Stack<Double> stk, int tipoOp) {
+		double topo1 = stk.pop(), topo2 = stk.pop();
+		if (tipoOp == 0) {
+			return topo2 * topo1;
+		} else if (tipoOp == 1) {
+			return topo2 + topo1;
+		} else if (tipoOp == 2) {
+			return topo2 / topo1;
+		} else if (tipoOp == 3) {
+			return topo2 - topo1;
+		} else {
+			return Math.pow(topo2, topo1);
+		}
 	}
 }
